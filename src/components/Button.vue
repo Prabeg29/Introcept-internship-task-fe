e86aad2b779e0d8c28b66dd1e5d99cb6f33fe2bc
Prabeg Shakya <shakyaprabeg@gmail.com>
2021-09-21 11:58:30 +0545

    Add button component

---
+++ src/components/Button.vue
@@ -0,0 +1,12 @@
+<template>
+    <button @click="$emit('btn-click')"> {{operator}} </button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        operator: String
+    }
+}
+</script>
