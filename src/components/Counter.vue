781af5519486edfd03c38f3045873a23dab431c1
Prabeg Shakya <shakyaprabeg@gmail.com>
2021-09-21 11:59:19 +0545

    Add counter component and implement button component in it

---
+++ src/components/Counter.vue
@@ -0,0 +1,36 @@
+<template>
+    <h2>Count: {{count}}</h2>
+    <div class="button">
+        <Button @btn-click= "increaseCount" operator="Up Count"/>
+        <Button @btn-click= "decreaseCount" operator="Down Count"/>
+        <Button @btn-click= "resetCount" operator="Reset"/>
+    </div>
+</template>
+
+<script>
+import Button from './Button.vue'
+
+export default {
+    name: 'Counter',
+    data() {
+        return {
+            count: 0
+        }
+    },
+    components: {
+        Button
+    },
+    methods: {
+        increaseCount(){
+            this.count++
+        },
+        decreaseCount(){
+            this.count--
+        },
+        resetCount(){
+            this.count = 0
+        }
+    }
+}
+</script>
+
